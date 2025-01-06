43547eda6b06ba1f0e3bbbf411b566d6b4c9413e
shuai <lishuailing@sifou.com>
2025-01-06 14:12:51 +0800

    fix: delete test data

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,9 +109,6 @@ const config = {
           {
             label: 'Plugins',
             to: '/plugins',
-            tos: {
-              'zh-CN': 'https://test.com'
-            }
           },
           {
             label: 'Blog',
@@ -133,9 +130,6 @@ const config = {
               {
                 label: 'Q&A Community',
                 to: 'https://meta.answer.dev',
-                tos: {
-                  'zh-CN': 'https://baidu.com',
-                }
               },
               {
                 label: 'Team',
